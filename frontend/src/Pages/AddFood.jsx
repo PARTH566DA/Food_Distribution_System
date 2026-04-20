ad5881c6d97e1ca48c9ea0c60f1ccd78c38c8fe9
PARTH566DA <202301022@daiict.ac.in>
2026-04-21 00:06:32 +0530

    Fixed: Map

--- frontend/src/Pages/AddFood.jsx
+++ frontend/src/Pages/AddFood.jsx
@@ -487,10 +487,9 @@ const AddFood = () => {
                   style={{ height: '100%', width: '100%' }}
                 >
                   <TileLayer
-                    url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                    subdomains="abcd"
-                    maxZoom={20}
+                    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                    attribution='Tiles &copy; Esri &mdash; Source: Esri, Maxar, Earthstar Geographics, and the GIS User Community'
+                    maxZoom={19}
                   />
                   <MapClickHandler onLocationSelect={handleMapLocationSelect} />
                   {formData.latitude && formData.longitude && (
